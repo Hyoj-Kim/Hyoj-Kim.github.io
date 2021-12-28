df7a1104e43601b0c961ce1e01eaba3d77f9c6af
Hyojeong Kim <42.4.hyojekim@gmail.com>
2021-12-28 15:38:52 +0900

    chore: Modify main.yml for github action
    npm install을 github action에 쓸 수 있는 npm ci로 변경
    gatsby-meta-config.js에서 깃허브 주소를 ID로 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteUrl: `https://hyoj-kim.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: `https://github.com/Hyoj-Kim`, // Your GitHub account
+    github: `Hyoj-Kim`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
